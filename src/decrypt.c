dd8112202b11efb6658a6bd61303d4e9a3d140d1
ktneale <kt.neale@gmail.com>
2015-09-25 12:26:12 +0100

    Further reformatting

--- src/decrypt.c
+++ src/decrypt.c
@@ -31,14 +31,14 @@ void crypto_init()
 int get_key(unsigned char *supplied_key, unsigned char *key)
 {
     if (!supplied_key || !key) {
-	    printf("Error! Bad key parameters!");
-	    return -1;
+        printf("Error! Bad key parameters!");
+        return -1;
     }
 
-    if (strlen(supplied_key) != AES_KEY_LENGTH * 2)	//64 character hex string represents 32 bytes (256 bits).
+    if (strlen(supplied_key) != AES_KEY_LENGTH * 2) //64 character hex string represents 32 bytes (256 bits).
     {
-	    printf("Error! Bad key length!");
-	    return -1;
+        printf("Error! Bad key length!");
+        return -1;
     }
 
     int i = 0, count = 0;
@@ -46,15 +46,15 @@ int get_key(unsigned char *supplied_key, unsigned char *key)
 
     //Convert the hex string into a byte array
     for (i = 0; i < AES_KEY_LENGTH; i++) {
-	    sscanf(pos, "%2hhx", &key[i]);
-	    pos += 2 * sizeof(char);
+        sscanf(pos, "%2hhx", &key[i]);
+        pos += 2 * sizeof(char);
     }
 
 #ifdef DEBUG
     printf("\n[KEY BYTE ARRAY - START]\n");
 
     for (i = 0; i < AES_KEY_LENGTH; i++) {
-	    printf("%02x ", key[i]);
+        printf("%02x ", key[i]);
     }
 
     printf("\n[KEY BYTE ARRAY - END]\n\n");
@@ -68,14 +68,14 @@ int get_key(unsigned char *supplied_key, unsigned char *key)
 int get_iv(unsigned char *supplied_iv, unsigned char *iv)
 {
     if (!supplied_iv || !iv) {
-	    printf("Error! Bad iv parameters!");
-	    return -1;
+        printf("Error! Bad iv parameters!");
+        return -1;
     }
 
-    if (strlen(supplied_iv) != AES_IV_LENGTH * 2)	//32 character hex string represents 16 bytes (128 bits).
+    if (strlen(supplied_iv) != AES_IV_LENGTH * 2)   //32 character hex string represents 16 bytes (128 bits).
     {
-	    printf("Error! Bad iv length!");
-	    return -1;
+        printf("Error! Bad iv length!");
+        return -1;
     }
 
     int i = 0, count = 0;
@@ -83,15 +83,15 @@ int get_iv(unsigned char *supplied_iv, unsigned char *iv)
 
     //Convert the hex string into a byte array
     for (i = 0; i < AES_IV_LENGTH; i++) {
-	    sscanf(pos, "%2hhx", &iv[i]);
-	    pos += 2 * sizeof(char);
+        sscanf(pos, "%2hhx", &iv[i]);
+        pos += 2 * sizeof(char);
     }
 
 #ifdef DEBUG
     printf("\n[IV BYTE ARRAY - START]\n");
 
     for (i = 0; i < AES_IV_LENGTH; i++) {
-	    printf("%02x ", iv[i]);
+        printf("%02x ", iv[i]);
     }
 
     printf("\n[IV BYTE ARRAY - END]\n\n");
@@ -102,9 +102,9 @@ int get_iv(unsigned char *supplied_iv, unsigned char *iv)
 
 
 int encrypt(unsigned char *plaintext,
-	    int plaintext_len,
-	    unsigned char *key,
-	    unsigned char *iv, unsigned char *ciphertext)
+        int plaintext_len,
+        unsigned char *key,
+        unsigned char *iv, unsigned char *ciphertext)
 {
     EVP_CIPHER_CTX *ctx;
 
@@ -116,7 +116,7 @@ int encrypt(unsigned char *plaintext,
 
     /* Create and initialise the context */
     if (!(ctx = EVP_CIPHER_CTX_new()))
-	handleErrors();
+    handleErrors();
 
     /* Initialise the encryption operation. IMPORTANT - ensure you use a key
      * and IV size appropriate for your cipher
@@ -124,20 +124,20 @@ int encrypt(unsigned char *plaintext,
      * IV size for *most* modes is the same as the block size. For AES this
      * is 128 bits */
     if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-	handleErrors();
+    handleErrors();
 
     /* Provide the message to be encrypted, and obtain the encrypted output.
      * EVP_EncryptUpdate can be called multiple times if necessary
      */
     if (1 !=EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-	handleErrors();
+    handleErrors();
     ciphertext_len = len;
 
     /* Finalise the encryption. Further ciphertext bytes may be written at
      * this stage.
      */
     if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
-	handleErrors();
+    handleErrors();
     ciphertext_len += len;
 
     /* Clean up */
@@ -147,9 +147,9 @@ int encrypt(unsigned char *plaintext,
 }
 
 int decrypt(unsigned char *ciphertext,
-	    int ciphertext_len,
-	    unsigned char *key,
-	    unsigned char *iv, unsigned char *plaintext)
+        int ciphertext_len,
+        unsigned char *key,
+        unsigned char *iv, unsigned char *plaintext)
 {
     EVP_CIPHER_CTX *ctx;
 
@@ -159,7 +159,7 @@ int decrypt(unsigned char *ciphertext,
 
     /* Create and initialise the context */
     if (!(ctx = EVP_CIPHER_CTX_new()))
-	handleErrors();
+    handleErrors();
 
     /* Initialise the decryption operation. IMPORTANT - ensure you use a key
      * and IV size appropriate for your cipher
@@ -167,21 +167,21 @@ int decrypt(unsigned char *ciphertext,
      * IV size for *most* modes is the same as the block size. For AES this
      * is 128 bits */
     if (1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-	handleErrors();
+    handleErrors();
 
     /* Provide the message to be decrypted, and obtain the plaintext output.
      * EVP_DecryptUpdate can be called multiple times if necessary
      */
     if (1 !=EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext,
-			  ciphertext_len))
-	handleErrors();
+              ciphertext_len))
+    handleErrors();
     plaintext_len = len;
 
     /* Finalise the decryption. Further plaintext bytes may be written at
      * this stage.
      */
     if (1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-	handleErrors();
+    handleErrors();
     plaintext_len += len;
 
     /* Clean up */
@@ -219,39 +219,39 @@ int main(int argc, char *argv[])
     printf("AES file decryption tool.  Written by Kevin Neale 2015\n\n");
 
     if (argc < 5 || !argv[1] || !argv[2] || !argv[3] || !argv[4]) {
-	printf("Usage: ./decrypt <in> <out> <key> <iv>\n\n");
-	printf("    <in>         - Filepath to the file to be decrypted\n");
-	printf("    <out>        - Filepath to the file which will contain the encrypted data\n\n");
-	printf("    <key>        - 256 bit key to be used for the encryption represented as a 64 character hex string\n");
-	printf("                   e.g. 471C6ABB3CAD5CD41509F961EDD3A2E08E76F61FF6D63412B79E9D500257A06A\n\n");
-	printf("    <iv>         - 128 bit initialisation vector to be used for the encryption represented as a 32 character hex string\n");
-	printf("                   e.g. 743D32B3BEDEEAF566BE6A444B89AA2E\n\n");
-	exit(EXIT_FAILURE);
+    printf("Usage: ./decrypt <in> <out> <key> <iv>\n\n");
+    printf("    <in>         - Filepath to the file to be decrypted\n");
+    printf("    <out>        - Filepath to the file which will contain the encrypted data\n\n");
+    printf("    <key>        - 256 bit key to be used for the encryption represented as a 64 character hex string\n");
+    printf("                   e.g. 471C6ABB3CAD5CD41509F961EDD3A2E08E76F61FF6D63412B79E9D500257A06A\n\n");
+    printf("    <iv>         - 128 bit initialisation vector to be used for the encryption represented as a 32 character hex string\n");
+    printf("                   e.g. 743D32B3BEDEEAF566BE6A444B89AA2E\n\n");
+    exit(EXIT_FAILURE);
     }
 
     //Open the input file
     FILE *in = fopen(argv[1], "r");
 
     if (!in) {
-	    printf("Error! Could not open input file\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Could not open input file\n");
+        exit(EXIT_FAILURE);
     }
     //Open the output file
     FILE *out = fopen(argv[2], "w");
 
     if (!out) {
-	    printf("Error! Could not open output file\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Could not open output file\n");
+        exit(EXIT_FAILURE);
     }
     //Get key and iv
     if (!out) {
-	    printf("Error! Could not open output file\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Could not open output file\n");
+        exit(EXIT_FAILURE);
     }
 
     if (get_key(argv[3], key) != 0 || get_iv(argv[4], iv) != 0) {
-	    printf("Error! Bad parameters\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Bad parameters\n");
+        exit(EXIT_FAILURE);
     }
 
     //Prepare input buffer
@@ -264,15 +264,15 @@ int main(int argc, char *argv[])
     fseek(in, 0L, SEEK_SET);
 
     if (ciphertext_len <= 0) {
-	    printf("Error! Input file is empty\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Input file is empty\n");
+        exit(EXIT_FAILURE);
     }
 
     ciphertext = malloc(sizeof(char) * ciphertext_len);
 
     if (!ciphertext) {
-	    printf("Error! Could not allocate memory for input buffer\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Could not allocate memory for input buffer\n");
+        exit(EXIT_FAILURE);
     }
 
     memset(ciphertext, '\0', sizeof(char) * ciphertext_len);
@@ -283,13 +283,13 @@ int main(int argc, char *argv[])
 
     //Prepare the output buffer 
 
-    plaintext_len = ciphertext_len;	//It certainly will be less or equal to the cipher text length.
+    plaintext_len = ciphertext_len; //It certainly will be less or equal to the cipher text length.
 
     plaintext = malloc(sizeof(char) * plaintext_len);
 
     if (!plaintext) {
-	    printf("Error! Could not allocate memory for output buffer\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Could not allocate memory for output buffer\n");
+        exit(EXIT_FAILURE);
     }
 
     memset(plaintext, '\0', sizeof(char) * plaintext_len);
@@ -297,16 +297,16 @@ int main(int argc, char *argv[])
 
     /* Decrypt the ciphertext */
     plaintext_len =
-	decrypt(ciphertext, ciphertext_len, key, iv, plaintext);
+    decrypt(ciphertext, ciphertext_len, key, iv, plaintext);
 
     printf("length of plaintext_len: %d\n", plaintext_len);
 
     for (i = 0; i < plaintext_len; i++) {
-	    if (plaintext[i] != '\0')	//No need to write out null characters.
-	        fwrite(&plaintext[i], sizeof(unsigned char), 1, out);
+        if (plaintext[i] != '\0')   //No need to write out null characters.
+            fwrite(&plaintext[i], sizeof(unsigned char), 1, out);
     }
 
-    sync();			//Force output flush
+    sync();         //Force output flush
 
     free(ciphertext);
     free(plaintext);
--- tests/test.c
+++ tests/test.c
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
     ret =system("./encrypt tests/king_lear_pt.txt output/new_king_lear_ct.txt 471C6ABB3CAD5CD41509F961EDD3A2E08E76F61FF6D63412B79E9D500257A06A 743D32B3BEDEEAF566BE6A444B89AA2E");
 
     if (ret != 0) {
-	    printf("Error! Step 1 failed, could not encrypt the input.\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Step 1 failed, could not encrypt the input.\n");
+        exit(EXIT_FAILURE);
     }
 
 
@@ -33,8 +33,8 @@ int main(int argc, char *argv[])
     ret =system("./decrypt output/new_king_lear_ct.txt output/new_king_lear_pt.txt 471C6ABB3CAD5CD41509F961EDD3A2E08E76F61FF6D63412B79E9D500257A06A 743D32B3BEDEEAF566BE6A444B89AA2E");
 
     if (ret != 0) {
-	    printf("Error! Step 2 failed, could not decrypt the input.\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Step 2 failed, could not decrypt the input.\n");
+        exit(EXIT_FAILURE);
     }
 
 
@@ -42,8 +42,8 @@ int main(int argc, char *argv[])
     ret =system("/usr/bin/diff tests/king_lear_pt.txt output/new_king_lear_pt.txt");
 
     if (ret != 0) {
-	    printf("Error! Step 3 failed, the new plaintext does not match the original plaintext.\n");
-	    exit(EXIT_FAILURE);
+        printf("Error! Step 3 failed, the new plaintext does not match the original plaintext.\n");
+        exit(EXIT_FAILURE);
     }
 
     printf("SUCCESS! Unit test passed.\n");
